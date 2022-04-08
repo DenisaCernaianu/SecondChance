6ca8c2503e13743e14d2b5d4ace011c344733b05
DenisaCernaianu <denisa.cernaianu@student.upt.ro>
2022-04-08 15:49:35 +0300

    Initial commit

---
+++ src/main/java/org/example/main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class main {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+        /*Logger LOGGER = LoggerFactory.getLogger(main.class);
+
+        LOGGER.trace("This is a trace log");
+        LOGGER.debug("This is a debug log");
+        LOGGER.info("This is a info log");
+        LOGGER.warn("This is a warn log");
+        LOGGER.error("This is an error log" );*/
+    }
+}
